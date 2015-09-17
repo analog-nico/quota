3f83bd94c6147c537836b8882871319b881ea8fd
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-17 16:08:45 +0200

    Implemented WindowSliding throttling

---
+++ test/spec/throttling/window-sliding.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var quota = require('../../../lib/index.js');
+
+var _ = require('lodash');
+var BPromise = require('bluebird');
+
+
+describe('Throttling WindowSliding', function () {
+
+    it('should validate the options', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'window-sliding'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return quotaClient.requestQuota('test')
+            .then(function () {
+                throw new Error('Expected an Error');
+            })
+            .catch(quota.OutOfQuotaError, function (err) {
+                throw new Error('Did not expect an OutOfQuotaError');
+            })
+            .catch(function (err) {
+                expect(err.message).to.eql('Please pass the window parameter to allow sliding window throttling');
+            });
+
+    });
+
+    it('1 request', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            window: 10,
+            throttling: 'window-sliding'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test');
+
+            })
+            .delay(5)
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function () {
+                        throw new Error('Expected OutOfQuotaError');
+                    })
+                    .catch(quota.OutOfQuotaError, function (err) {
+                        return; // Expected
+                    });
+
+            })
+            .delay(5)
+            .then(function () {
+
+                return quotaClient.requestQuota('test');
+
+            });
+
+    });
+
+    it('2 requests', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 2,
+            window: 10,
+            throttling: 'window-sliding'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test');
+
+            })
+            .delay(5)
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, 2)
+                    .then(function () {
+                        throw new Error('Expected OutOfQuotaError');
+                    })
+                    .catch(quota.OutOfQuotaError, function (err) {
+                        return; // Expected
+                    });
+
+            })
+            .then(function () {
+
+                return quotaClient.requestQuota('test');
+
+            })
+            .delay(5)
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, 2)
+                    .then(function () {
+                        throw new Error('Expected OutOfQuotaError');
+                    })
+                    .catch(quota.OutOfQuotaError, function (err) {
+                        return; // Expected
+                    });
+
+            })
+            .delay(5)
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, 2);
+
+            });
+
+    });
+
+});
