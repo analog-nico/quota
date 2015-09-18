38115d5bc95586ce618d779fa27a4301c8f9bacd
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-19 00:03:44 +0200

    Improved queueing and its test coverage

--- test/spec/queueing/fifo.js
+++ test/spec/queueing/fifo.js
@@ -35,12 +35,195 @@ describe('Queueing Fifo', function () {
 
                 setTimeout(function () {
                     firstGrant.dismiss();
-                });
+                }, 10);
 
                 return quotaClient.requestQuota('test')
                     .then(function (grant) {
                         grant.dismiss();
                     });
+
+            });
+
+    });
+
+    it('2 requests', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 2,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        var grant1, grant2, counter = 0;
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        grant1 = grant;
+                    });
+
+            })
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        grant2 = grant;
+                    });
+
+            })
+            .then(function (firstGrant) {
+
+                setTimeout(function () {
+                    grant1.dismiss();
+                }, 10);
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test')
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(1);
+                            grant.dismiss();
+                        }),
+                    quotaClient.requestQuota('test')
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(2);
+                            grant.dismiss();
+                        })
+                ]);
+
+            })
+            .then(function () {
+                grant2.dismiss();
+            });
+
+    });
+
+    it('waiting again for second resource', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo',
+            resource: 'res1'
+        });
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo',
+            resource: 'res2'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        var grant1, grant2, counter = 0;
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, { res1: 1 })
+                    .then(function (grant) {
+                        grant1 = grant;
+                    });
+
+            })
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, { res2: 1 })
+                    .then(function (grant) {
+                        grant2 = grant;
+                    });
+
+            })
+            .then(function (firstGrant) {
+
+                setTimeout(function () {
+                    counter += 1;
+                    grant1.dismiss();
+                }, 10);
+
+                setTimeout(function () {
+                    counter += 1;
+                    grant2.dismiss();
+                }, 20);
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test', undefined, { res1: 1, res2: 1 })
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(3);
+                            grant.dismiss();
+                        }),
+                    quotaClient.requestQuota('test', undefined, { res1: 1 })
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(4);
+                            grant.dismiss();
+                        })
+                ]);
+
+            });
+
+    });
+
+    it('no overtaking', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        var grant1, counter = 0;
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, 0.5)
+                    .then(function (grant) {
+                        grant1 = grant;
+                    });
+
+            })
+            .then(function () {
+
+                setTimeout(function () {
+                    counter += 1;
+                    grant1.dismiss();
+                }, 10);
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test', undefined, 1)
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(2);
+                            grant.dismiss();
+                        }),
+                    quotaClient.requestQuota('test', undefined, 0.5)
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(3);
+                            grant.dismiss();
+                        })
+                ]);
 
             });
 
