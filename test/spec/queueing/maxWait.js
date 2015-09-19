25ccf272932eb2cfbbc443e42561f55716fca788
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-19 17:22:51 +0200

    Tests for maxWait parameter

---
+++ test/spec/queueing/maxWait.js
@@ -0,0 +1,175 @@
+'use strict';
+
+var quota = require('../../../lib/index.js');
+
+var _ = require('lodash');
+var BPromise = require('bluebird');
+
+
+describe('Queueing with maxWait', function () {
+
+    it('timing out', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        return grant;
+                    });
+
+            })
+            .then(function (firstGrant) {
+
+                setTimeout(function () {
+                    firstGrant.dismiss();
+                }, 10);
+
+                return quotaClient.requestQuota('test', undefined, undefined, { maxWait: 0 })
+                    .then(function () {
+                        throw new Error('Expected OutOfQuotaError');
+                    })
+                    .catch(quota.OutOfQuotaError, function (err) {
+                        return; // Expected
+                    });
+
+            });
+
+    });
+
+    it('timing out (multiple)', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        return grant;
+                    });
+
+            })
+            .then(function (firstGrant) {
+
+                setTimeout(function () {
+                    firstGrant.dismiss();
+                }, 10);
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 0 })
+                        .then(function () {
+                            throw new Error('Expected OutOfQuotaError');
+                        })
+                        .catch(quota.OutOfQuotaError, function (err) {
+                            return; // Expected
+                        }),
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 5 })
+                        .then(function () {
+                            throw new Error('Expected OutOfQuotaError');
+                        })
+                        .catch(quota.OutOfQuotaError, function (err) {
+                            return; // Expected
+                        }),
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 10 })
+                        .then(function (grant) {
+                            // Expected
+                            grant.dismiss();
+                        })
+                ]);
+
+            });
+
+    });
+
+    it('timing out at the right time', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        var counter = 0;
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        return grant;
+                    });
+
+            })
+            .then(function (firstGrant) {
+
+                setTimeout(function () {
+                    counter += 1;
+                }, 5);
+
+                setTimeout(function () {
+                    counter += 1;
+                }, 7);
+
+                setTimeout(function () {
+                    firstGrant.dismiss();
+                }, 10);
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 0 })
+                        .then(function () {
+                            throw new Error('Expected OutOfQuotaError');
+                        })
+                        .catch(quota.OutOfQuotaError, function (err) {
+                            counter += 1;
+                            expect(counter).to.eql(1);
+                        }),
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 5 })
+                        .then(function () {
+                            throw new Error('Expected OutOfQuotaError');
+                        })
+                        .catch(quota.OutOfQuotaError, function (err) {
+                            counter += 1;
+                            expect(counter).to.eql(3);
+                        }),
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 10 })
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(5);
+                            grant.dismiss();
+                        })
+                ]);
+
+            });
+
+    });
+
+});
