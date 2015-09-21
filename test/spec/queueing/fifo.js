13c232534cde0161806a601f82d4550035cb4d23
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-21 10:36:00 +0200

    Adding again queued request or processing more

--- test/spec/queueing/fifo.js
+++ test/spec/queueing/fifo.js
@@ -163,13 +163,13 @@ describe('Queueing Fifo', function () {
                     quotaClient.requestQuota('test', undefined, { res1: 1, res2: 1 })
                         .then(function (grant) {
                             counter += 1;
-                            expect(counter).to.eql(3);
+                            expect(counter).to.eql(4);
                             grant.dismiss();
                         }),
                     quotaClient.requestQuota('test', undefined, { res1: 1 })
                         .then(function (grant) {
                             counter += 1;
-                            expect(counter).to.eql(4);
+                            expect(counter).to.eql(2);
                             grant.dismiss();
                         })
                 ]);
@@ -228,5 +228,109 @@ describe('Queueing Fifo', function () {
             });
 
     });
+
+    it('no overtaking when not enough available at first', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 3,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        var counter = 0;
+
+        return BPromise.all([
+            quotaClient.requestQuota('test'),
+            quotaClient.requestQuota('test'),
+            quotaClient.requestQuota('test')
+        ])
+            .spread(function (grant1, grant2, grant3) {
+
+                setTimeout(function () {
+                    counter += 1;
+                    grant1.dismiss();
+                });
+
+                setTimeout(function () {
+                    counter += 1;
+                    grant2.dismiss();
+                }, 5);
+
+                setTimeout(function () {
+                    counter += 1;
+                    grant2.dismiss();
+                }, 10);
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test', undefined, 2, { maxWait: 10 })
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(3);
+                            grant.dismiss();
+                        }),
+                    quotaClient.requestQuota('test', undefined, 1, { maxWait: 10 })
+                        .then(function (grant) {
+                            counter += 1;
+                            expect(counter).to.eql(4);
+                            grant.dismiss();
+                        })
+                ]);
+
+            });
+
+    });
+
+    it('granting as many as possible when more available', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            limit: 2,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, 2)
+                    .then(function (grant) {
+                        return grant;
+                    });
+
+            })
+            .then(function (firstGrant) {
+
+                setTimeout(function () {
+                    firstGrant.dismiss();
+                });
+
+                return BPromise.all([
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 10 }),
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 10 }),
+                    quotaClient.requestQuota('test', undefined, undefined, { maxWait: 10 })
+                        .then(function () {
+                            throw new Error('Expected OutOfQuotaError');
+                        })
+                        .catch(quota.OutOfQuotaError, function (err) {
+                            return; // Expected
+                        })
+                ]);
+
+            });
+
+    });
+
+    it('granting second in line after first in line timed out');
 
 });
