3580bbaa97d7afbf35b4ab46652b374abb8ad4c2
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-22 23:25:47 +0200

    Tested and fixed Facebook preset

---
+++ test/spec/presets/facebook.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var quota = require('../../../lib/index.js');
+
+var _ = require('lodash');
+var BPromise = require('bluebird');
+
+
+describe('Preset Facebook', function () {
+
+    it('should grant unlimited quota for the Graph API', function () {
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('facebook');
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.all([
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook'),
+            quotaClient.requestQuota('facebook')
+        ]);
+
+    });
+
+    it('should grant unlimited quota for requests to the marketing API', function () {
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('facebook', { api: 'marketing' });
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.all([
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 }),
+            quotaClient.requestQuota('facebook', {}, { requests: 999 })
+        ]);
+
+    });
+
+    it('should grant 4 budgetChange requests to the Marketing API', function () {
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('facebook', { api: 'marketing' });
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.all([
+            quotaClient.requestQuota('facebook', {}, { budgetChange: 1 }),
+            quotaClient.requestQuota('facebook', {}, { budgetChange: 1 }),
+            quotaClient.requestQuota('facebook', {}, { budgetChange: 1 }),
+            quotaClient.requestQuota('facebook', {}, { budgetChange: 1 }),
+            quotaClient.requestQuota('facebook', {}, { budgetChange: 1 }, { maxWait: 0 })
+                .then(function () {
+                    throw new Error('Expected OutOfQuotaError');
+                })
+                .catch(quota.OutOfQuotaError, function (err) {
+                    return; // Expected
+                })
+        ]);
+
+    });
+
+});
