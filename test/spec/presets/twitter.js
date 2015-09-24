65fd80b01733b08889e2d98d46e1f58d7c99bdc4
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-24 19:24:26 +0200

    Basic test

---
+++ test/spec/presets/twitter.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var quota = require('../../../lib/index.js');
+
+var _ = require('lodash');
+var BPromise = require('bluebird');
+
+
+describe('Preset Twitter', function () {
+
+    it('should grant quota for individual endpoints and each user', function () {
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('twitter');
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.all([
+            quotaClient.requestQuota('twitter', { userId: 1 }, { 'account/settings': 15 }, { maxWait: 0 }),
+            quotaClient.requestQuota('twitter', { userId: 1 }, { 'account/settings': 15 }, { maxWait: 0 })
+                .then(function () {
+                    throw new Error('Expected OutOfQuotaError');
+                })
+                .catch(quota.OutOfQuotaError, function (err) {
+                    return; // Expected
+                }),
+            quotaClient.requestQuota('twitter', { userId: 2 }, { 'account/settings': 15 }, { maxWait: 0 }),
+            quotaClient.requestQuota('twitter', { userId: 2 }, { 'account/settings': 15 }, { maxWait: 0 })
+                .then(function () {
+                    throw new Error('Expected OutOfQuotaError');
+                })
+                .catch(quota.OutOfQuotaError, function (err) {
+                    return; // Expected
+                }),
+            quotaClient.requestQuota('twitter', { userId: 1 }, { 'account/verify_credentials': 15 }, { maxWait: 0 }),
+            quotaClient.requestQuota('twitter', { userId: 1 }, { 'account/verify_credentials': 15 }, { maxWait: 0 })
+                .then(function () {
+                    throw new Error('Expected OutOfQuotaError');
+                })
+                .catch(quota.OutOfQuotaError, function (err) {
+                    return; // Expected
+                })
+        ]);
+
+    });
+
+});
