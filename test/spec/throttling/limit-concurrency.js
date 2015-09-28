04fac9e2f4be3b91613d747c866e1855802da9df
analog-nico <nicolai.kamenzky@testrails.org>
2015-09-28 11:33:34 +0200

    Allowing to change the limit of limit-concurrency throttling

--- test/spec/throttling/limit-concurrency.js
+++ test/spec/throttling/limit-concurrency.js
@@ -8,6 +8,31 @@ var BPromise = require('bluebird');
 
 describe('Throttling LimitConcurrency', function () {
 
+    it('should validate options.limit', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            throttling: 'limit-concurrency'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return quotaClient.requestQuota('test')
+            .then(function () {
+                throw new Error('Expected an Error');
+            })
+            .catch(quota.OutOfQuotaError, function (err) {
+                throw new Error('Did not expect an OutOfQuotaError');
+            })
+            .catch(function (err) {
+                expect(err.message).to.eql('Please pass the limit parameter to allow limit-concurrency throttling');
+            });
+
+    });
+
     it('1 request', function () {
 
         var quotaManager = new quota.Manager();
@@ -129,6 +154,110 @@ describe('Throttling LimitConcurrency', function () {
                     .then(function (thirdGrant) {
                         secondGrant.dismiss();
                         thirdGrant.dismiss();
+                    });
+
+            });
+
+    });
+
+    it('should allow increasing the limit', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            name: 'main',
+            limit: 1,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        var _grant;
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        _grant = grant;
+                    });
+
+            })
+            .then(function () {
+
+                setTimeout(function () {
+                    _grant.dismiss({
+                        forRule: {
+                            main: {
+                                limit: 2
+                            }
+                        }
+                    });
+                }, 5);
+
+                return quotaClient.requestQuota('test');
+
+            })
+            .then(function () {
+
+                return quotaClient.requestQuota('test');
+
+            })
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, undefined, { maxWait: 0 })
+                    .then(function () {
+                        throw new Error('Expected OutOfQuotaError');
+                    })
+                    .catch(quota.OutOfQuotaError, function (err) {
+                        return; // Expected
+                    });
+
+            });
+
+    });
+
+    it('should allow decreasing the limit', function () {
+
+        var quotaManager = new quota.Manager();
+        quotaManager.addRule({
+            name: 'main',
+            limit: 2,
+            throttling: 'limit-concurrency',
+            queueing: 'fifo'
+        });
+
+        var quotaServer = new quota.Server();
+        quotaServer.addManager('test', quotaManager);
+
+        var quotaClient = new quota.Client(quotaServer);
+
+        return BPromise.resolve()
+            .then(function () {
+
+                return quotaClient.requestQuota('test')
+                    .then(function (grant) {
+                        grant.dismiss({
+                            forRule: {
+                                main: {
+                                    limit: 1
+                                }
+                            }
+                        });
+                    });
+
+            })
+            .then(function () {
+
+                return quotaClient.requestQuota('test', undefined, 2, { maxWait: 0 })
+                    .then(function () {
+                        throw new Error('Expected OutOfQuotaError');
+                    })
+                    .catch(quota.OutOfQuotaError, function (err) {
+                        return; // Expected
                     });
 
             });
